4d082349ebca7ea761798ef359cf36eb543d0e00
Kiritee <kiritee.amruth@innovaccer.com>
2017-04-21 18:36:37 +0530

    installs needed

---
+++ installs.sh
@@ -0,0 +1,9 @@
+printf "\ninstalling pymongo..\n"
+sudo -H pip install pymongo
+printf "\npymongo installed"
+printf "\nInstalling BeautifulSoup4..\n"
+sudo -H pip install bs4
+printf "\nBeautifulSoup4 installed"
+printf "\nInstalling nltk..\n"
+sudo -H pip install nltk
+printf "\nBase nltk packages installed. Download stopwords and other requirements being used.\n"
